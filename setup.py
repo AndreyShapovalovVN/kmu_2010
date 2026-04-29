00c396ac072a4778a1d10afb2ca66ec89022dc74
Andrey <a147@ukr.net>
2026-04-29 08:46:36 +0200

    Rename package from UkrTranslit to ukr-translit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='UkrTranslit',
+    name='ukr-translit',
     version='0.1.0',
     description='Transliteration cyrilic to latn',
     packages=find_packages(),
